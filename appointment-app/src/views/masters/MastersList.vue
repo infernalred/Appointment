1ec45ac4278691a16489049a6590eafd399210b4
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-09-09 19:49:31 +0300

    try work with app

---
+++ appointment-app/src/views/masters/MastersList.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Мастера</h1>
+</template>
+
+<script>
+export default {
+  name: "MastersList",
+};
+</script>
+
+<style scoped></style>
---
+++ appointment-app/src/views/services/ServicesList.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Услуги</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ServicesList",
+};
+</script>
+
+<style scoped></style>
