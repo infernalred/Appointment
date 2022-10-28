815dc2febe92d051aea9adb58ceb37a4f226f466
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-10-28 12:07:49 +0300

    Update DashboardMyAppointments.tsx

--- appointment-app/src/features/dashboard/DashboardMyAppointments.tsx
+++ appointment-app/src/features/dashboard/DashboardMyAppointments.tsx
@@ -1,34 +1,34 @@
 import React, { useEffect } from "react";
-import {observer} from "mobx-react-lite";
-import {Table} from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import { Table } from "semantic-ui-react";
 import { useStore } from "../../app/store/store";
 
 export default observer(function DashboardMyAppointments() {
-    const {appointmentStore} = useStore();
-    const {appointments, loadMyAppointmentsByDate} = appointmentStore;
+  const { appointmentStore } = useStore();
+  const { appointments, loadMyAppointmentsByDate } = appointmentStore;
 
-    useEffect(() => {
-        loadMyAppointmentsByDate();
-    }, [loadMyAppointmentsByDate])
+  useEffect(() => {
+    loadMyAppointmentsByDate();
+  }, [loadMyAppointmentsByDate]);
 
-    return (
-        <>
-            <Table celled>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Имя</Table.HeaderCell>
-                        <Table.HeaderCell>Время</Table.HeaderCell>
-                        <Table.HeaderCell>Действия</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                {appointments.map(ap => (
-                    <Table.Row key={ap.id}>
-                        <Table.Cell>{ap.userName}</Table.Cell>
-                    </Table.Row>
-                ))}
-            </Table.Body>
-            </Table>
-        </>
-    )
-})
+  return (
+    <>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Имя</Table.HeaderCell>
+            <Table.HeaderCell>Время</Table.HeaderCell>
+            <Table.HeaderCell>Действия</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {appointments.map((ap) => (
+            <Table.Row key={ap.id}>
+              <Table.Cell>{ap.userName}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </>
+  );
+});
