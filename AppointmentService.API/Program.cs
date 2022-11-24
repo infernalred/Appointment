998e6bc87f627f5e973a41c1cddab6a0726e546e
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-24 12:19:45 +0300

    add Strict-Transport-Security policy

--- AppointmentService.API/Program.cs
+++ AppointmentService.API/Program.cs
@@ -56,7 +56,11 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseHsts();
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy");
